c34834dc51f6f6c536f975b1d86b6d5792a8e537
AdamoHamou <Hamoua2@unlv.nevada.edu>
2025-03-22 23:57:13 -0700

    Added files to improve workflow for the website designers.

--- website/client/src/App.js
+++ website/client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import NavBar from "./comp/NavBar";
+import About from "./comp/about";
+import Prompt from "./comp/prompt";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/prompt" element={<Prompt />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ website/client/src/comp/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Write about the project here including all contributors
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Write about the project here</p>
+        </div>
+    );
+}
+
+export default About;
