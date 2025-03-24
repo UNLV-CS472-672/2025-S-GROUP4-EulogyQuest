22ba859978aabde021f3c3280439f04467e79665
Parham Pahlavan <palavanparham@gmail.com>
2025-03-24 01:09:21 -0700

    added navbar functionality

--- website/client/src/comp/NavBar.js
+++ website/client/src/comp/NavBar.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 // make nav bar look nice @Parham HAHAHA add a css file and include here please :D
 function NavBar() {
     return (
-        <nav>
-        <ul>
-            <li>
-            <Link to="/prompt">Prompt</Link>
-            </li>
-            <li>
-            <Link to="/about">About</Link>
-            </li>
-        </ul>
-        </nav>
+        <div className="navbar">
+            <Link to="/about" >About</Link>
+            <Link to="/prompt" class = "active">Prompt</Link>
+        </div>
     );
 }
 
 export default NavBar;
+
