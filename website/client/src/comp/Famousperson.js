18303477ce76bf018463e32a66fdf55d39bba194
AdamoHamou <Hamoua2@unlv.nevada.edu>
2025-04-03 21:07:18 -0700

    Added subnavbar within Gereate quest based off if the person is famous or not

---
+++ website/client/src/comp/Famousperson.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SubNavBar from './SubNavBar';
+
+
+function FamousPerson() {
+    
+    return (
+        <div>
+            <SubNavBar />
+            <h1>Famous Person</h1>
+        </div>
+    );
+}
+
+export default FamousPerson;
