f7c46fa3d19b4dc656b8ab61c691c845549f6707
Parham Pahlavan <palavanparham@gmail.com>
2025-04-06 20:43:26 -0700

    Added Honored One section (Not fully functional yet)

--- website/client/src/comp/Honoredone.js
+++ website/client/src/comp/Honoredone.js
@@ -1,14 +1,45 @@
 import React from "react";
 import SubNavBar from "./SubNavBar";
 
+/* ai-gen start (ChatGPT-4, 2) */
 function HonoredOne() {
+
+    const [name, setName] = React.useState("");
+
+    const handleSubmit = () => {
+        console.log("Submitted name:", name);
+    };
 
     return (
         <div>
             <SubNavBar />
-            <h1>Honored One</h1>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "50vh", textAlign: "center" }}>
+                <h1>Honored One</h1>
+                <p>Please enter your Honored One's name below:</p>
+                <input
+                    type="text"
+                    placeholder="Enter the name of the Honored One here"
+                    value={name || ""}
+                    onChange={(e) => setName(e.target.value)}
+                    style={{ width: "300px", padding: "10px", fontSize: "16px", marginBottom: "20px" }}
+                />
+                <button onClick={handleSubmit}>Submit</button>
+
+                <div style={{ marginTop: "20px" }}>
+                    <input
+                        type="file"
+                        accept=".txt"
+                        onChange={(e) => {
+                            if (e.target.files.length > 0) {
+                                console.log("File selected:", e.target.files[0].name);
+                            }
+                        }}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
 
 export default HonoredOne;
+/*  ai-gen end */
