893807f3f45a04fa8b98331d17a4dd2dda4441c6
Parham Pahlavan <palavanparham@gmail.com>
2025-04-08 00:00:48 -0700

    Added 'Honored One' sub page support. Also unified the output file of both famous person and honored one sections. the output file is called before_ai.json

--- website/client/src/comp/Honoredone.js
+++ website/client/src/comp/Honoredone.js
@@ -1,17 +1,44 @@
 import React from "react";
 import SubNavBar from "./SubNavBar";
-
+import axios from 'axios';
 
 /* ai-gen start (ChatGPT-4, 2) */
 function HonoredOne() {
 
     const [name, setName] = React.useState("");
     const [file, setFile] = React.useState(null);
+    const [text, setText] = React.useState('');
+    const [message_name, setMessage_name] = React.useState("");
+    const [message_logs, setMessage_logs] = React.useState('');
 
-    const handleSubmit = () => {
-        console.log("Submitted name:", name);
-        console.log("Submitted file:", file.name);
+    const handleSubmit = async () => {
+        if (!name) {
+            console.error("Please input a name.");
+        }
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = async (e) => {
+                const fileContent = e.target.result;
+                setText(fileContent);
+
+                try {
+                    const response = await axios.post("http://localhost:5000/honored_one", {
+                        message_name: name,
+                        message_logs: fileContent,
+                    });
+                    console.log("Response from server:", response.data);
+                } catch (error) {
+                    console.error("Error sending prompt:", error);
+                }
+            };
+            reader.readAsText(file);
+        } else {
+            setText('Please select a valid .txt file.');
+            console.error("Please select a valid .txt file.");
+        }
     };
+
 
     return (
         <div>
